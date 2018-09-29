894a1d79ba31236e6267d819bdb063832901e94a
Germán Lugo <german.lugo@bitnibs.com>
2018-09-29 16:11:11 +0200

    Deployment ready.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ChocoToken
+Initial version of ChocoToken to be deployed in the Rinkeby ethereum testnet.
--- truffle.js
+++ truffle.js
@@ -2,12 +2,9 @@ require('dotenv').config();
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     rinkeby: {
       provider: function() {
-        console.log(process.env.MNEMONIC);
         return new HDWalletProvider(
           process.env.MNEMONIC,
           `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`
@@ -19,7 +16,7 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*"
     }
   },
   solc: {
